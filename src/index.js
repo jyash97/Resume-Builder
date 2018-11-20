70444f0664a812b05c422083e5637b89c7e8593d
jyash97 <jyash97@gmail.com>
2018-11-20 17:18:28 +0530

    Test env variables

--- src/index.js
+++ src/index.js
@@ -1,22 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-import App from './App';
-import Resume from './Resume';
-import Instructions from './Instructions';
-import registerServiceWorker from './registerServiceWorker';
+import App from "./App";
+import Resume from "./Resume";
+import Instructions from "./Instructions";
+import registerServiceWorker from "./registerServiceWorker";
+
+console.log(process.env);
 
 const Routes = () => (
   <Router>
     <Switch>
-      <Route exact path='/' component={App} />
-      <Route exact path='/instructions' component={Instructions} />
-      <Route exact path='/resume' component={Resume} />
+      <Route exact path="/" component={App} />
+      <Route exact path="/instructions" component={Instructions} />
+      <Route exact path="/resume" component={Resume} />
       <Route component={App} />
     </Switch>
   </Router>
-)
+);
 
-ReactDOM.render(<Routes />, document.getElementById('root'));
+ReactDOM.render(<Routes />, document.getElementById("root"));
 registerServiceWorker();
